d5fd327a6b6c12b96e24aac5b725df7c7697d20e
Yulong Wang <yl.wang@mail.nankai.edu.cn>
2024-02-01 11:46:32 +0800

    fix: Merged MultiStepForecastDataset

--- datasets/MultiStepForecastDataset.py
+++ datasets/MultiStepForecastDataset.py
@@ -9,7 +9,7 @@ class MultiStepForecastDataset(AbstractDataset):
     Single step forecasting is a special case of multi-step forecasting when set forecast_len=1.
     """
 
-    def __init__(self, data, history_len, forecast_len, *args, **kwargs):
+    def __init__(self, data, history_len, forecast_len, long_history_len=None, *args, **kwargs):
         """
         Initialize the MultiStepForecastDataset.
 
@@ -25,6 +25,8 @@ class MultiStepForecastDataset(AbstractDataset):
             The length of the historical data sequence.
         forecast_len : int
             The length of the forecast data sequence.
+        long_history_len : int, optional
+            The length of the long historical data sequence for pretrain (default is None).
         """
         super().__init__(data, *args, **kwargs)
         if len(data.shape) == 2:
@@ -32,6 +34,7 @@ class MultiStepForecastDataset(AbstractDataset):
         self.data = data
         self.history_len = history_len
         self.forecast_len = forecast_len
+        self.long_history_len = long_history_len
 
     def __getitem__(self, index):
         """
@@ -48,13 +51,37 @@ class MultiStepForecastDataset(AbstractDataset):
             A tuple containing two tensors: the historical data (x) with dimensions (history_len, N, C) and the forecast
             data (y) with dimensions (forecast_len, N, C), both as torch.float32 tensors.
         """
-        x = self.data[index : index + self.history_len]
-        y = self.data[
-            index + self.history_len : index + self.history_len + self.forecast_len
-        ]
-        return torch.tensor(x, dtype=torch.float32), torch.tensor(
-            y, dtype=torch.float32
-        )
+        if self.long_history_len is not None:
+            long_history_data = self.data[index: index + self.long_history_len]
+            history_data = self.data[
+                           index
+                           + self.long_history_len
+                           - self.history_len: index
+                                               + self.long_history_len
+                           ]
+            future_data = self.data[
+                          index
+                          + self.long_history_len: index
+                                                   + self.long_history_len
+                                                   + self.forecast_len
+                          ]
+
+            return (
+                torch.from_numpy(history_data).type(torch.float32),
+                torch.from_numpy(future_data).type(torch.float32),
+                torch.from_numpy(long_history_data).type(torch.float32),
+            )
+        else:
+            x = self.data[index : index + self.history_len]
+            y = self.data[
+                index + self.history_len : index + self.history_len + self.forecast_len
+            ]
+            return torch.tensor(x, dtype=torch.float32), torch.tensor(
+                y, dtype=torch.float32
+            )
 
     def __len__(self):
-        return self.data.shape[0] - self.history_len - self.forecast_len + 1
+        if self.long_history_len is not None:
+            return self.data.shape[0] - self.long_history_len - self.forecast_len + 1
+        else:
+            return self.data.shape[0] - self.history_len - self.forecast_len + 1
