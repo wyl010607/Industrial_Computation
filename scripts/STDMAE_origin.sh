b8d491d4ae0d8d1d33c2b3036764ee84652d0e8b
Yulong Wang <yl.wang@mail.nankai.edu.cn>
2024-03-01 22:01:26 +0800

    fix: typo

--- scripts/STDMAE_origin.sh
+++ scripts/STDMAE_origin.sh
@@ -1,7 +1,7 @@
 echo "开始测试......"
 python ./main.py \
     --data_config_path ./config/data_config/DIST_with_pretrain_config.yaml \
-    --model_config_path ./config/model_config/pretrain_model_config.ymal \
+    --model_config_path ./config/model_config/pretrain_model_config.yaml \
     --train_config_path ./config/train_config/train_with_STDMAE_pretrain_config.yaml \
     --model_name STDMAE \
     --model_save_path ./model_states/jl_STDMAE_origin.pkl \
