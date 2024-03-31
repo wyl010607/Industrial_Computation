cb308baf200099629d3e236f2856151bdbedfd46
Yulong Wang <yl.wang@mail.nankai.edu.cn>
2024-03-31 19:54:10 +0800

    feat: update datasets

--- data_preprocessors/__init__.py
+++ data_preprocessors/__init__.py
@@ -1,5 +1,23 @@
 from .abs import AbstractDataPreprocessor
 from .DCSDataPreprocessor import DCSDataPreprocessor
 from .ASPENDataPreprocessor import ASPENDataPreprocessor
+from .AirQualityDataPreprocessor import AirQualityDataPreprocessor
+from .ElectricityDataPreprocessor import ElectricityDataPreprocessor
+from .ETTDataPreprocessor import ETTDataPreprocessor
+from .ExchangeRateDataPreprocessor import ExchangeRateDataPreprocessor
+from .LargeSTDataPreprocessor import LargeSTDataPreprocessor
+from .PEMSDataPreprocessor import PEMSDataPreprocessor
+from .WeatherDataPreprocessor import WeatherDataPreprocessor
 
-__all__ = ["AbstractDataPreprocessor", "DCSDataPreprocessor", "ASPENDataPreprocessor"]
+__all__ = [
+    "AbstractDataPreprocessor",
+    "DCSDataPreprocessor",
+    "ASPENDataPreprocessor",
+    "AirQualityDataPreprocessor",
+    "ElectricityDataPreprocessor",
+    "ETTDataPreprocessor",
+    "ExchangeRateDataPreprocessor",
+    "LargeSTDataPreprocessor",
+    "PEMSDataPreprocessor",
+    "WeatherDataPreprocessor",
+]
