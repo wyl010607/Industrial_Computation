8de2e4fb8940ae5fb7c119599d35c0a1c4428879
lmy020418 <109254980+lmy020418@users.noreply.github.com>
2024-06-04 12:01:18 +0800

    GDN时间数据处理类
    GDN时间数据处理类

--- datasets/TimeDataset.py
+++ datasets/TimeDataset.py
@@ -1,22 +1,22 @@
 import torch
 from torch.utils.data import Dataset, DataLoader
+from .abs import AbstractDataset
 import torch.nn.functional as F
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
 import numpy as np
 
 
-class TimeDataset(Dataset):
-    def __init__(self, raw_data, edge_index, mode='train', slide_window=5, slide_stride=1, *args, **kwargs):
-        super().__init__(raw_data, slide_window, slide_stride, *args, **kwargs)
+class TimeDataset(AbstractDataset):
+    def __init__(self, raw_data, edge_index, slide_win, slide_stride, mode='train', *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.raw_data = raw_data
+        self.slide_win = slide_win
+        self.slide_stride = slide_stride
         self.edge_index = edge_index
         self.mode = mode
         x_data = raw_data[:-1]
         labels = raw_data[-1]
         data = x_data
-        self.slide_window = slide_window
-        self.slide_stride = slide_stride
-        self.update_dataset_params["slide_window"] = slide_window
-        self.update_dataset_params["slide_stride"] = slide_stride
         # to tensor
         data = torch.tensor(data).double()
         labels = torch.tensor(labels).double()
@@ -25,17 +25,10 @@ class TimeDataset(Dataset):
     def __len__(self):
         return len(self.x)
 
-    def __getitem__(self, idx):
-        feature = self.x[idx].double()
-        y = self.y[idx].double()
-        edge_index = self.edge_index.long()
-        label = self.labels[idx].double()
-        return feature, y, label, edge_index
-
     def process(self, data, labels):
         x_arr, y_arr = [], []
         labels_arr = []
-        slide_win, slide_stride = [self.slide_window, self.slide_stride]
+        slide_win, slide_stride = self.slide_win, self.slide_stride
         is_train = self.mode == 'train'
         node_num, total_time_len = data.shape
         rang = range(slide_win, total_time_len, slide_stride) if is_train else range(slide_win, total_time_len)
@@ -49,3 +42,10 @@ class TimeDataset(Dataset):
         y = torch.stack(y_arr).contiguous()
         labels = torch.Tensor(labels_arr).contiguous()
         return x, y, labels
+
+    def __getitem__(self, idx):
+        feature = self.x[idx].double()
+        y = self.y[idx].double()
+        edge_index = self.edge_index.long()
+        label = self.labels[idx].double()
+        return feature, y, label, edge_index
