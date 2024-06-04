b13786ac0793c4198e8e5a222eb5c5c310999094
lmy020418 <109254980+lmy020418@users.noreply.github.com>
2024-06-04 12:03:45 +0800

    GDN训练模块更新
    GDN训练模块更新

--- trainers/GDNTrainer.py
+++ trainers/GDNTrainer.py
@@ -1,68 +1,59 @@
 from tqdm import tqdm
 import torch
-from trainers.abs import AbstractTrainer
+import torch.nn as nn
 import torch.nn.functional as F
-import time
 import numpy as np
-from utils.metrics import get_full_err_scores,get_best_performance_data
+import os
+import time
+import math
+from sklearn.metrics import precision_recall_fscore_support, roc_auc_score
+from trainers.abs import AbstractTrainer
+from utils.early_stop import EarlyStopping
+from utils.metrics import smooth
 
-_device = None
+from sklearn.metrics import precision_recall_fscore_support
+from sklearn.metrics import accuracy_score
+import warnings
 
-def get_device():
-    return _device
+warnings.filterwarnings('ignore')
 
-device = get_device()
+
+def asMinutes(s):
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
+
+def timeSincePlus(since, percent):
+    now = time.time()
+    s = now - since
+    es = s / (percent)
+    rs = es - s
+    return '%s (- %s)' % (asMinutes(s), asMinutes(rs))
 
 
 class GDNTrainer(AbstractTrainer):
-    """
-    A Trainer subclass for sGDN model.
-    """
-
     def __init__(
-        self,
-        model,
-        optimizer,
-        scheduler,
-        scaler,
-        model_save_path,
-        result_save_dir_path,
-        max_epoch_num,
-        enable_early_stop=False,
-        early_stop_patience=5,
-        early_stop_min_is_best=True,
-        *args,
-        **kwargs,
+            self,
+            model,
+            optimizer,
+            scaler,
+            scheduler,
+            model_save_path,
+            result_save_dir_path,
+            max_epoch_num,
+            res_pth,
+            enable_early_stop=False,
+            early_stop_patience=3,
+            early_stop_min_is_best=True,
+            *args,
+            **kwargs,
     ):
-        """
-        Parameters
-        ----------
-        model : torch.nn.Module
-            The neural network model for single step forecasting.
-        optimizer : torch.optim.Optimizer
-            The optimizer to use for training the model.
-        scheduler : torch.optim.lr_scheduler
-            The learning rate scheduler.
-        scaler : Scaler
-            Scaler object used for normalizing and denormalizing data.
-        model_save_path : str
-            Path to save the trained model.
-        result_save_dir_path : str
-            Directory path to save training and evaluation results.
-        max_epoch_num : int
-            The maximum number of epochs for training.
-        enable_early_stop : bool, optional
-            Whether to enable early stopping (default is False).
-        early_stop_patience : int, optional
-            Number of epochs with no improvement after which training will be stopped (default is 5).
-        early_stop_min_is_best : bool, optional
-            Flag to indicate if lower values of loss indicate better performance (default is True).
-        """
         super().__init__(
             model,
             optimizer,
-            scheduler,
             scaler,
+            scheduler,
             model_save_path,
             result_save_dir_path,
             max_epoch_num,
@@ -75,33 +66,56 @@ class GDNTrainer(AbstractTrainer):
 
     def loss_func(self, y_pred, y_true, *args, **kwargs):
         loss = F.mse_loss(y_pred, y_true, reduction='mean')
+
         return loss
 
-    def train_one_epoch(self, data_loader, *args, **kwargs):
+    def train_one_epoch(self, train_loader, val_loader, epoch, *args, **kwargs):
+        path = self.model_save_path
         self.model.train()
-        total_loss = 0
-        # tqmd_ = tqdm(data_loader)
+        i = 0
+
+        acu_loss = 0
         train_loss_list = []
-        for x, labels, attack_labels, edge_index in data_loader:
+        for x, labels, attack_labels, edge_index in train_loader:
             _start = time.time()
-            x, labels, edge_index = [item.float().to(device) for item in [x, labels, edge_index]]
+            x, labels, edge_index = [item.float().to(self.device) for item in [x, labels, edge_index]]
             self.optimizer.zero_grad()
-            out = self.model(x, edge_index).float().to(device)
+            out = self.model(x, edge_index).float().to(self.device)
             loss = self.loss_func(out, labels)
+
             loss.backward()
             self.optimizer.step()
+
             train_loss_list.append(loss.item())
-            total_loss += loss.item()
-        return total_loss / len(data_loader)
+            acu_loss += loss.item()
+            i+=1
+            if i % 200 == 0:
+                avg_loss = acu_loss / i
+                print(f"Train loss: {avg_loss:.4f}")
+        print('epoch ({} / {}) (Loss:{:.8f}, ACU_loss:{:.8f})'.format(
+            epoch, self.max_epoch_num,
+            acu_loss / len(train_loader), acu_loss), flush=True)
+        return acu_loss
 
     def evaluate(self, data_loader, metrics, save_graph=False, *args, **kwargs):
-        self.model.eval()
-        y_true, y_pred, tol_loss, data_num = [], [], 0, 0
+        # self.model.eval()
         test_loss_list = []
+        now = time.time()
+        test_predicted_list = []
+        test_ground_list = []
+        test_labels_list = []
+        t_test_predicted_list = []
+        t_test_ground_list = []
+        t_test_labels_list = []
+        test_len = len(data_loader)
+        self.model.eval()
+
+        i = 0
+        acu_loss = 0
         for x, y, labels, edge_index in data_loader:
-            x, y, labels, edge_index = [item.to(device).float() for item in [x, y, labels, edge_index]]
+            x, y, labels, edge_index = [item.to(self.device).float() for item in [x, y, labels, edge_index]]
             with torch.no_grad():
-                predicted = self.model(x, edge_index).float().to(device)
+                predicted = self.model(x, edge_index).float().to(self.device)
                 loss = self.loss_func(predicted, y)
                 labels = labels.unsqueeze(1).repeat(1, predicted.shape[1])
                 if len(t_test_predicted_list) <= 0:
@@ -114,20 +128,13 @@ class GDNTrainer(AbstractTrainer):
                     t_test_labels_list = torch.cat((t_test_labels_list, labels), dim=0)
 
             test_loss_list.append(loss.item())
-            tol_loss += loss.item()
+            acu_loss += loss.item()
+            i += 1
+            if i % 1000 == 1 and i > 1:
+                print(timeSincePlus(now, i / test_len))
         test_predicted_list = t_test_predicted_list.tolist()
         test_ground_list = t_test_ground_list.tolist()
         test_labels_list = t_test_labels_list.tolist()
         avg_loss = sum(test_loss_list) / len(test_loss_list)
-        return avg_loss, test_predicted_list, test_ground_list, test_labels_list
 
-    def get_score(self, test_result, val_result):
-        np_test_result = np.array(test_result)
-        test_labels = np_test_result[2, :, 0].tolist()
-        test_scores, normal_scores = get_full_err_scores(test_result, val_result)
-        top1_best_info = get_best_performance_data(test_scores, test_labels, topk=1)
-        return top1_best_info
-
-    def get_gdn_score(self,test_result, val_result):
-        f1, prec, recall, auc = self.get_score(test_result, val_result)
-        return f1,prec,recall,auc
+        return avg_loss, [test_predicted_list, test_ground_list, test_labels_list]
